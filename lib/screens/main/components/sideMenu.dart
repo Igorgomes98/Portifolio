14b1a1a0a8970da1fcf3ce194a8f4f647377e7f6
Igor Augusto Gomes <43591315+IgorGomesFATEC@users.noreply.github.com>
2023-11-22 08:20:44 -0300

    Update sideMenu.dart

--- lib/screens/main/components/sideMenu.dart
+++ lib/screens/main/components/sideMenu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portifolio/screens/main/components/myInfo.dart';
 
 class sideMenu extends StatelessWidget {
   const sideMenu({
@@ -9,35 +10,7 @@ class sideMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(children: [
-        AspectRatio(
-          aspectRatio: 1.23,
-          child: Container(
-            color: const Color(0xFF242430),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Spacer(
-                  flex: 2,
-                ),
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage("images/IMG_8281.jpg"),
-                ),
-                Spacer(),
-                Text(
-                  "Igor Gomes",
-                  style: Theme.of(context).textTheme.titleSmall,
-                ),
-                Text("Desenvolvedor Web Fullstack \ne Mobile",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.w200, height: 1.5)),
-                Spacer(
-                  flex: 2,
-                ),
-              ],
-            ),
-          ),
-        )
+        MyInfo()
       ]),
     );
   }
