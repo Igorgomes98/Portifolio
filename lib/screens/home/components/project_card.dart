1e9ecdcea86a3d1151a39be0c913a0153d9310f5
Igor Augusto Gomes <iagomes95@gmail.com>
2024-02-05 20:35:24 -0300

    feat(projects): Build a GridView of projects
    - Build a GridView of projects (only UI).
    - Consume Model simulated backend

---
+++ lib/screens/home/components/project_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:portifolio/constants.dart';
+import 'package:portifolio/models/Project.dart';
+
+class ProjectCard extends StatelessWidget {
+  const ProjectCard({
+    super.key,
+    required this.project,
+  });
+  final Project project;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(defaultPadding),
+      color: secondaryColor,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Text(
+          project.title!,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.titleSmall,
+        ),
+        Spacer(),
+        Text(
+          project.description!,
+          maxLines: 4,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(height: 1.5),
+        ),
+        Spacer(),
+        TextButton(
+          
+          onPressed: () {},
+          child: Text(
+            'Read More >>',
+            style: TextStyle(
+              color: primaryColor,
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:portifolio/constants.dart';
+import 'package:portifolio/models/Project.dart';
 import 'package:portifolio/screens/home/components/home_banner.dart';
 import 'package:portifolio/screens/home/components/label_counter_info.dart';
+import 'package:portifolio/screens/home/components/project_card.dart';
 import 'package:portifolio/screens/main/main_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,10 +11,28 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MainScreen(
+    return MainScreen(
       children: [
         HomeBanner(),
         LabelCounterAnimatedInfo(),
+        Text(
+          "Meus Projetos",
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        const SizedBox(height: defaultPadding,),
+        GridView.builder(
+          shrinkWrap: true,
+          itemCount: demoProjects.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: defaultPadding,
+            childAspectRatio: 1.3,
+            mainAxisSpacing: defaultPadding,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return ProjectCard(project: demoProjects[index]);
+          },
+        ),
       ],
     );
   }
