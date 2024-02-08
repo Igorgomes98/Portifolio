90b8002c58d023fb23d92b2685394424402c90f6
Igor Augusto Gomes <iagomes95@gmail.com>
2024-02-08 18:23:51 -0300

    feat(exclude id): Exclude id projects
    - exclude id projects
    - added a http for wakatime
    - change the number counting for a time of languages
    - Create a new model Language

--- lib/http/webclients/git_webclient.dart
+++ lib/http/webclients/git_webclient.dart
@@ -1,18 +1,55 @@
 import 'package:dio/dio.dart';
+import 'package:portifolio/models/Language.dart';
 import 'package:portifolio/models/Project.dart';
 
 class GitWebClient {
-  final String githubAPIRepo = "https://api.github.com/users/igorgomes98/repos";
   Future<List<Project>?> getGitProjects() async {
+    const String githubAPIRepo =
+        "https://api.github.com/users/igorgomes98/repos";
     final Dio dio = Dio();
     final Response response = await dio.get(githubAPIRepo);
     if (response.statusCode == 200) {
       final List<dynamic>? responseData = response.data;
       if (responseData != null) {
-      final List<Project> returnProject = responseData.map((dynamic json) => Project.fromJson(json)).toList();
-      return returnProject;
+        final List<int> notIn = [745042621, 493246735];
+        final List<Project> allProjects = responseData
+            .where((json) => !notIn.contains(json['id']))
+            .map((dynamic json) => Project.fromJson(json))
+            .toList();
+        return allProjects;
       }
     }
     return [];
+  }
+
+  Future<List<Language>?> getWakaLanguageTime() async {
+    try {
+      const String wakaAPILanguage =
+          "https://wakatime.com/share/@igorgomes98/15927b97-0ae9-4a5a-8b46-e3710246b923.json";
+      final Dio dio = Dio();
+      final Response response = await dio.get(wakaAPILanguage);
+      if (response.statusCode == 200) {
+        final List<dynamic>? responseData = response.data['data'];
+        if (responseData != null) {
+          final List<Language> languages = responseData
+              .map((dynamic json) => Language.fromJson(json))
+              .toList()
+              .sublist(0, 4);
+          return languages;
+        }
+      }
+      return [];
+    } on DioException catch (e) {
+      if (e.response != null) {
+          print(e.response!.data);
+          print(e.response!.headers);
+          print(e.response!.requestOptions);
+        } else {
+          // Something happened in setting up or sending the request that triggered an Error
+          print(e.requestOptions);
+          print(e.message);
+        }
+    }
+    return null;
   }
 }
--- lib/screens/home/components/label_counter_info.dart
+++ lib/screens/home/components/label_counter_info.dart
@@ -1,62 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:portifolio/components/animated_counter.dart';
 import 'package:portifolio/constants.dart';
+import 'package:portifolio/http/webclients/git_webclient.dart';
+import 'package:portifolio/models/Language.dart';
 import 'package:portifolio/responsive.dart';
 import 'package:portifolio/screens/home/components/label_counter.dart';
 
-class LabelCounterAnimatedInfo extends StatelessWidget {
+class LabelCounterAnimatedInfo extends StatefulWidget {
   const LabelCounterAnimatedInfo({
     super.key,
   });
+
+  @override
+  State<LabelCounterAnimatedInfo> createState() =>
+      _LabelCounterAnimatedInfoState();
+}
+
+class _LabelCounterAnimatedInfoState extends State<LabelCounterAnimatedInfo> {
+  List<Language> languages = [];
+  final GitWebClient _gitWebClient = GitWebClient();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLanguages();
+  }
+
+  void getLanguages() async {
+    try {
+      final List<Language>? response =
+          await _gitWebClient.getWakaLanguageTime();
+      setState(() {
+        languages = response!;
+      });
+      print(languages.toString());
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: defaultPadding),
       child: Responsive.isMobileLarge(context)
-          ? const Column(
+          ? Column(
               children: [
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    LabelCounterAnimated(
-                        counter: AnimatedCounter(value: 119, text: 'K+'),
-                        label: "Subscribers"),
-                    LabelCounterAnimated(
-                        counter: AnimatedCounter(value: 40, text: '+'),
-                        label: "Videos"),
-                  ],
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: languages
+                        .map((language) {
+                          return LabelCounterAnimated(
+                            counter: AnimatedCounter(
+                                value: language.hours, text: 'h+'),
+                            label: language.name,
+                          );
+                        })
+                        .toList()
+                        .sublist(0, 2)),
+                SizedBox(
+                  height: defaultPadding,
                 ),
-                SizedBox(height: defaultPadding,),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    LabelCounterAnimated(
-                        counter: AnimatedCounter(value: 30, text: '+'),
-                        label: "Github Projects"),
-                    LabelCounterAnimated(
-                        counter: AnimatedCounter(value: 13, text: 'K+'),
-                        label: "Stars"),
-                  ],
-                )
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: languages
+                        .map((language) {
+                          return LabelCounterAnimated(
+                            counter: AnimatedCounter(
+                                value: language.hours, text: 'h+'),
+                            label: language.name,
+                          );
+                        })
+                        .toList()
+                        .sublist(2, 4))
               ],
             )
-          : const Row(
+          : Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                LabelCounterAnimated(
-                    counter: AnimatedCounter(value: 119, text: 'K+'),
-                    label: "Subscribers"),
-                LabelCounterAnimated(
-                    counter: AnimatedCounter(value: 40, text: '+'),
-                    label: "Videos"),
-                LabelCounterAnimated(
-                    counter: AnimatedCounter(value: 30, text: '+'),
-                    label: "Github Projects"),
-                LabelCounterAnimated(
-                    counter: AnimatedCounter(value: 13, text: 'K+'),
-                    label: "Stars"),
-              ],
+              children: languages.map((language) {
+                return LabelCounterAnimated(
+                  counter: AnimatedCounter(value: language.hours, text: 'h+'),
+                  label: language.name,
+                );
+              }).toList(),
             ),
     );
   }
